05bb79974004f28cc074b6e9747a2c92b7ede1ac
Gus <gustavonips@gmail.com>
2025-08-31 19:43:49 -0300

    instance: fix connect response interface

--- src/modules/instance/schemas/connect.ts
+++ src/modules/instance/schemas/connect.ts
@@ -1,28 +1,13 @@
 // Pure TypeScript interfaces for better IDE support and performance
-import { ConnectionState } from "@/types/webhooks";
-
 export interface ConnectRequest {
   instanceName: string;
 }
 
-export interface ConnectInstanceInfo {
-  instanceName: string;
-  state: ConnectionState;
-}
-
-export interface ConnectHash {
-  apikey: string;
-}
-
-export interface ConnectQRCode {
-  code: string;
-  base64?: string; // The doc doesn't specify, but it's good to have
-}
-
 export interface ConnectResponse {
-  instance: ConnectInstanceInfo;
-  hash: ConnectHash;
-  qrcode: ConnectQRCode;
+  pairingCode: string | null;
+  code: string;
+  base64: string;
+  count: number;
 }
 
 // Backward compatibility aliases
